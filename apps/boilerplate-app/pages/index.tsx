5034aec7216cee644b51eca9c7bd306a470908ea
mkosir <kosir.mar@gmail.com>
2022-08-26 14:53:53 +0200

    chore: update app

--- apps/boilerplate-app/pages/index.tsx
+++ apps/boilerplate-app/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+// import { Button } from "ui";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
-      <Button />
+      {/* <Button /> */}
     </div>
   );
 }
