b3df55c03bf14f41bad74128d01da60e9610832e
mkosir <kosir.mar@gmail.com>
2022-09-05 13:21:33 +0200

    chore(deps): update TypeScript

--- packages/jest-config-base/jest.config.js
+++ packages/jest-config-base/jest.config.js
@@ -3,16 +3,6 @@ const jestConfig = {
   preset: 'ts-jest',
   moduleDirectories: ['node_modules', 'src'],
   setupFilesAfterEnv: ['./jest.setup.ts'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
-  coverageThreshold: {
-    global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
-    },
-  },
 };
 
 module.exports = jestConfig;
