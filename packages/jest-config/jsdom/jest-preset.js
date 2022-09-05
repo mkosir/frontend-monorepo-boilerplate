2818085c4b732fc1fff74c403e20cad4af9ffc42
mkosir <kosir.mar@gmail.com>
2022-09-05 16:14:41 +0200

    chore(deps): update TypeScript

--- packages/jest-config/jsdom/jest-preset.js
+++ packages/jest-config/jsdom/jest-preset.js
@@ -1,6 +1,10 @@
 const jestConfig = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
+  // Required despite ts-jest preset
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'ts-jest',
+  },
   moduleDirectories: ['node_modules'],
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
 };
