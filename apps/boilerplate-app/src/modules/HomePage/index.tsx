81f11943c754fc5b5434bdb8ba015b60c8a471df
mkosir <kosir.mar@gmail.com>
2022-09-07 15:59:44 +0200

    chore(deps): update TypeScript

--- apps/boilerplate-app/src/modules/HomePage/index.tsx
+++ apps/boilerplate-app/src/modules/HomePage/index.tsx
@@ -1,12 +1,8 @@
 import { Button } from 'ui';
 
-import { ButtonsHome } from './components/ButtonsHome';
+import { MontyHome } from './components/MontyHome';
 
 export const HomePage = () => {
-  const name = 'test12';
-
-  const test = `test333${name}` || 4;
-  console.log('🔎 Log ~ HomePage ~ test', test);
   // if (test === null) {
   //   return <div></div>;
   // }
@@ -16,8 +12,14 @@ export const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
-      <Button text="Boop" onClick={() => console.log('Boop')} />
-      <ButtonsHome title="Home page feature - Buttons Stack" isTiltEnabled={true} />
+      <Button text="Echo" onClick={() => console.log('Echo')} />
+      <MontyHome
+        title="Home page (specific) feature"
+        isTiltEnabled={true}
+        onMontyHomeSelected={(isHome) =>
+          isHome ? console.log('Congrats') : console.log('Sorry, better luck next time')
+        }
+      />
     </div>
   );
 };
