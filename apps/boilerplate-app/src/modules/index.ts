e438fa6072a718a585149e9a4ce3478d603cd1d4
mkosir <kosir.mar@gmail.com>
2022-09-11 16:29:31 +0200

    chore(deps): update TypeScript

--- apps/boilerplate-app/src/modules/index.ts
+++ apps/boilerplate-app/src/modules/index.ts
@@ -1,6 +1,3 @@
 export * from './HomePage';
-export * from './MontyHomeMUIPage';
-export * from './MontyHomeTailwindPage';
-export * from './MontyHomeVanillaPage';
 export * from './Status404Page';
 export * from './Status500Page';
