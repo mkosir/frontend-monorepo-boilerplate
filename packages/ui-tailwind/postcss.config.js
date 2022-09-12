5598ecf7133d5a8fe8714457ab0af9f5eb1c102a
mkosir <kosir.mar@gmail.com>
2022-09-12 09:55:29 +0200

    chore(deps): update TypeScript

--- packages/ui-tailwind/postcss.config.js
+++ packages/ui-tailwind/postcss.config.js
@@ -1,6 +1,5 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const config = require('tailwind-config/postcss.config.js');
+
+module.exports = config;
