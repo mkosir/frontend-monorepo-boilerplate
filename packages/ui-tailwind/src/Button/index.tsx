331240b49c40a358760b18a33283bb9795483a78
mkosir <kosir.mar@gmail.com>
2022-09-15 15:53:33 +0200

    chore(deps): update TypeScript

--- packages/ui-tailwind/src/Button/index.tsx
+++ packages/ui-tailwind/src/Button/index.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable tailwindcss/classnames-order */
+// Tailwindcss eslint rule doesn't work correctly with Prettier
+
 export type ButtonProps = {
   text: string;
   className?: string;
@@ -11,7 +14,7 @@ export const Button = ({ text, className, onClick }: ButtonProps) => {
       className={`
       ${
         className ?? ''
-      }from-brand-blue to-brand-green hover:from-primary-600 rounded bg-gradient-to-r px-6 py-2.5 text-xs font-medium leading-tight text-white shadow-md transition duration-200 ease-in-out hover:scale-110 hover:to-green-500 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg`}
+      }from-brand-blue hover:from-primary-600 to-brand-green  rounded bg-gradient-to-r px-6 py-2.5 text-xs font-medium leading-tight text-white shadow-md transition duration-200 ease-in-out hover:scale-110 hover:to-green-500 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg`}
     >
       {text}
     </button>
