48d088564e04e3a6a7ceb64490b9f905cefe039c
mkosir <kosir.mar@gmail.com>
2022-09-18 13:25:14 +0200

    chore(deps): update TypeScript

--- apps/boilerplate-app-mui/src/pages/_app.tsx
+++ apps/boilerplate-app-mui/src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { CacheProvider } from '@emotion/react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme, createEmotionCache } from 'mui-config';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { theme, createEmotionCache } from 'ui-mui';
 
 const clientSideEmotionCache = createEmotionCache();
 
--- packages/ui-mui/src/config/index.ts
+++ packages/ui-mui/src/config/index.ts
@@ -1,2 +1,3 @@
 export * from './createEmotionCache';
 export * from './mui-theme';
+export * from './theme';
