c52f72dff876b5d02c4ce8c523205a1d0be5e5a2
mkosir <kosir.mar@gmail.com>
2022-09-19 13:39:55 +0200

    chore(deps): update TypeScript

--- packages/jest-config-base/jest.config.jsdom.ts
+++ packages/jest-config-base/jest.config.jsdom.ts
@@ -3,9 +3,11 @@ import { Config } from 'jest';
 export const jestConfigJsdom: Config = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
+  roots: ['<rootDir>/src/'],
+  modulePaths: ['<rootDir>/src/'],
   moduleDirectories: ['node_modules'],
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: `${__dirname}/../tsconfig/tsconfig.test.json` }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
   },
 };
