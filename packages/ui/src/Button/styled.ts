9922d7de0f329adeebd8d92edc15303e2dbb8970
mkosir <kosir.mar@gmail.com>
2022-09-19 09:22:20 +0200

    chore(deps): update TypeScript

--- packages/ui/src/Button/styled.ts
+++ packages/ui/src/Button/styled.ts
@@ -2,7 +2,9 @@ import { Button, styled } from '@mui/material';
 
 import { ButtonProps } from './';
 
-export const ButtonStyled = styled(Button)<Omit<ButtonProps, 'text'>>(({ theme, isDisabled, size, bgColor }) => ({
+export const ButtonStyled = styled(Button, {
+  shouldForwardProp: (prop) => prop !== 'isDisabled' && prop !== 'bgColor',
+})<Omit<ButtonProps, 'text'>>(({ theme, isDisabled, size, bgColor }) => ({
   width: isDisabled ? '100%' : undefined,
   color: size === 'small' ? theme.palette.secondary.main : undefined,
   backgroundColor: bgColor,
