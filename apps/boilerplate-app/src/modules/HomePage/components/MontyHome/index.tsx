9887b2316d38b1c639d9c94f8ddbd919414af412
mkosir <kosir.mar@gmail.com>
2022-09-20 13:49:12 +0200

    chore(deps): update TypeScript

--- apps/boilerplate-app/src/modules/HomePage/components/MontyHome/index.tsx
+++ apps/boilerplate-app/src/modules/HomePage/components/MontyHome/index.tsx
@@ -26,30 +26,28 @@ export const MontyHome = ({ title, isTiltEnabled, onMontyHomeSelected }: MontyHo
 
   return (
     <Tilt tiltEnable={isTiltEnabled} tiltMaxAngleX={20} tiltMaxAngleY={20} scale={1.03} style={{ maxWidth: '400px' }}>
-      <div>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            backgroundColor: '#64b0ce17',
-            margin: '14px',
-            padding: '13px',
-            borderRadius: '5px',
-            boxShadow: '0 4px 8px 0 #3d9cc23b',
-          }}
-        >
-          <h3>{title}</h3>
-          <a href="https://en.wikipedia.org/wiki/Monty_Hall_problem" target="_blank" rel="noreferrer">
-            Monty Home Problem
-          </a>
-          <div style={{ display: 'flex', justifyContent: 'space-between', width: '60%', marginTop: '12px' }}>
-            <Button onClick={() => handleMontyHomeSelected('goat')}>Home 1</Button>
-            <Button bgColor={theme.brand.green} onClick={() => handleMontyHomeSelected('home')}>
-              Home 2
-            </Button>
-            <Button onClick={() => handleMontyHomeSelected('goat')}>Home 3</Button>
-          </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          backgroundColor: '#64b0ce17',
+          margin: '14px',
+          padding: '13px',
+          borderRadius: '5px',
+          boxShadow: '0 4px 8px 0 #3d9cc23b',
+        }}
+      >
+        <h3>{title}</h3>
+        <a href="https://en.wikipedia.org/wiki/Monty_Hall_problem" target="_blank" rel="noreferrer">
+          Monty Home Problem
+        </a>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '60%', marginTop: '12px' }}>
+          <Button onClick={() => handleMontyHomeSelected('goat')}>Home 1</Button>
+          <Button bgColor={theme.brand.green} onClick={() => handleMontyHomeSelected('home')}>
+            Home 2
+          </Button>
+          <Button onClick={() => handleMontyHomeSelected('goat')}>Home 3</Button>
         </div>
       </div>
     </Tilt>
