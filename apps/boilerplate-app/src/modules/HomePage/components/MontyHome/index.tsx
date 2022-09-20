09f81a18e950d58529a631fa95ee5a93cb576191
mkosir <kosir.mar@gmail.com>
2022-09-20 14:03:58 +0200

    chore(deps): update TypeScript

--- apps/boilerplate-app/src/modules/HomePage/components/MontyHome/index.tsx
+++ apps/boilerplate-app/src/modules/HomePage/components/MontyHome/index.tsx
@@ -27,7 +27,7 @@ export const MontyHome = ({ title, isTiltEnabled, onMontyHomeSelected }: MontyHo
   return (
     <Tilt tiltEnable={isTiltEnabled} tiltMaxAngleX={20} tiltMaxAngleY={20} scale={1.03} style={{ maxWidth: '600px' }}>
       <Box
-        style={{
+        sx={{
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
@@ -42,7 +42,7 @@ export const MontyHome = ({ title, isTiltEnabled, onMontyHomeSelected }: MontyHo
         <a href="https://en.wikipedia.org/wiki/Monty_Hall_problem" target="_blank" rel="noreferrer">
           Monty Home Problem
         </a>
-        <Box style={{ display: 'flex', justifyContent: 'space-between', width: '60%', marginTop: '12px' }}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '60%', marginTop: '12px' }}>
           <Button onClick={() => handleMontyHomeSelected('goat')}>Home 1</Button>
           <Button bgColor={theme.brand.green} onClick={() => handleMontyHomeSelected('home')}>
             Home 2
