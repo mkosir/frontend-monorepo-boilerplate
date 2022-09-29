6974eaa9ac4f92452e38f01d234fcd7c39e46c3a
mkosir <kosir.mar@gmail.com>
2022-09-29 14:24:11 +0200

    chore(deps): update TypeScript

--- packages/config-jest/jest.config.jsdom.ts
+++ packages/config-jest/jest.config.jsdom.ts
@@ -8,6 +8,6 @@ export const jestConfigJsdom: Config = {
   moduleDirectories: ['node_modules'],
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }],
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.test.json' }],
   },
 };
