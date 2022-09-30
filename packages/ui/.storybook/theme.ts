d02272350a3ddaf14e163bda90f6ff4985cae5f8
mkosir <kosir.mar@gmail.com>
2022-09-30 22:56:18 +0200

    chore: upgrade CI to Node v18

--- packages/ui/.storybook/theme.ts
+++ packages/ui/.storybook/theme.ts
@@ -1,7 +1,9 @@
 import { create } from '@storybook/theming';
 
+import { getVersionInfo } from '../version/getVersionInfo';
+
 export const theme = create({
   base: 'light',
-  brandTitle: 'Turborepo Boilerplate - UI',
+  brandTitle: `Turborepo Boilerplate - UI ${getVersionInfo()}`,
   brandUrl: 'https://github.com/mkosir/turborepo-boilerplate',
 });
---
+++ packages/ui/version/getVersionInfo.ts
@@ -0,0 +1,16 @@
+import { VersionInfo } from './VersionInfo';
+
+export const getVersionInfo = (): string => {
+  if (VersionInfo.tag) {
+    return VersionInfo.tag;
+  }
+
+  if (VersionInfo.commit) {
+    let branchCommit = '';
+    branchCommit = VersionInfo.branch ? VersionInfo.branch : '';
+    branchCommit += VersionInfo.commit ? ` [${VersionInfo.commit.substring(0, 8)}]` : '';
+    return branchCommit;
+  }
+
+  return 'Local build';
+};
