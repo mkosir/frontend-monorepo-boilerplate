bf92d0253290bcceddbf7ecf31f39c4339ccf311
mkosir <kosir.mar@gmail.com>
2022-09-30 23:54:35 +0200

    chore: upgrade CI to Node v18

--- apps/boilerplate-app/src/modules/HomePage/index.tsx
+++ apps/boilerplate-app/src/modules/HomePage/index.tsx
@@ -10,7 +10,7 @@ export const HomePage = () => {
   // }
 
   return (
-    <Box m={5} bgcolor="purple">
+    <Box m={5}>
       <Typography variant="h5">Home Page</Typography>
       <MontyHome
         title="Home page (specific) feature"
