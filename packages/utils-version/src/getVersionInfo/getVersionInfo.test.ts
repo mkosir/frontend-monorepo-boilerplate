c21668baa72548d379b3275a7af17cd7fb127231
mkosir <kosir.mar@gmail.com>
2022-10-02 10:07:54 +0200

    chore(deps): update TypeScript

--- packages/utils-version/src/getVersionInfo/getVersionInfo.test.ts
+++ packages/utils-version/src/getVersionInfo/getVersionInfo.test.ts
@@ -1,8 +1,16 @@
+import { VersionInfo } from '../VersionInfo';
+
 import { getVersionInfo } from '.';
 
 describe('packages', () => {
   describe('utils-version', () => {
     describe('getVersionInfo', () => {
+      beforeEach(() => {
+        VersionInfo.tag = '';
+        VersionInfo.branch = '';
+        VersionInfo.commit = '';
+      });
+
       it('should return as local build when no tab, branch or commit is injected in VersionInfo.ts file', () => {
         expect(getVersionInfo()).toBe<string>('Local build');
       });
