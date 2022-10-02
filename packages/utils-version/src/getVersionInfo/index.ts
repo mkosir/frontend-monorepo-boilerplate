edecede066b94528d981785ec099fc9f16a50b3e
mkosir <kosir.mar@gmail.com>
2022-10-02 10:28:23 +0200

    chore(deps): update TypeScript

--- packages/utils-version/src/getVersionInfo/index.ts
+++ packages/utils-version/src/getVersionInfo/index.ts
@@ -8,10 +8,7 @@ export const getVersionInfo: GetVersionInfo = () => {
   }
 
   if (VersionInfo.commit) {
-    let branchCommit = '';
-    branchCommit = VersionInfo.branch ? VersionInfo.branch : '';
-    branchCommit += VersionInfo.commit ? ` [${VersionInfo.commit.substring(0, 7)}]` : '';
-    return branchCommit;
+    return `${VersionInfo.branch} [${VersionInfo.commit.substring(0, 7)}]`;
   }
 
   return 'Local build';
