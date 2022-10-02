008bb4274417d6754a7c368ae5f93784644d8c33
mkosir <kosir.mar@gmail.com>
2022-10-02 20:57:48 +0200

    chore(deps): update TypeScript

--- packages/ui/src/Version/index.tsx
+++ packages/ui/src/Version/index.tsx
@@ -8,7 +8,7 @@ export const Version = ({ version }: VersionProps) => {
   const theme = useTheme();
 
   return (
-    <Typography variant="caption" ml={0.5} sx={{ fontSize: 11, fontStyle: 'italic', color: theme.palette.grey[500] }}>
+    <Typography variant="caption" ml={0.5} sx={{ fontSize: 12, fontStyle: 'italic', color: theme.palette.grey[600] }}>
       {version}
     </Typography>
   );
