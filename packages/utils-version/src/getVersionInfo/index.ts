708460279911448a9d6566467daced0f2a6d71aa
mkosir <kosir.mar@gmail.com>
2022-10-02 10:30:13 +0200

    chore(deps): update TypeScript

--- packages/utils-version/src/getVersionInfo/index.ts
+++ packages/utils-version/src/getVersionInfo/index.ts
@@ -3,11 +3,11 @@ import { VersionInfo } from '../VersionInfo';
 type GetVersionInfo = () => string;
 
 export const getVersionInfo: GetVersionInfo = () => {
-  if (VersionInfo.tag) {
+  if (VersionInfo.tag !== '') {
     return VersionInfo.tag;
   }
 
-  if (VersionInfo.commit) {
+  if (VersionInfo.commit !== '') {
     return `${VersionInfo.branch} [${VersionInfo.commit.substring(0, 7)}]`;
   }
 
