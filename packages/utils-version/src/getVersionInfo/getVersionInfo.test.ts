c4576ce26301d6d48173966839c7b24e587cc617
mkosir <kosir.mar@gmail.com>
2022-10-02 10:24:30 +0200

    chore(deps): update TypeScript

--- packages/utils-version/src/getVersionInfo/getVersionInfo.test.ts
+++ packages/utils-version/src/getVersionInfo/getVersionInfo.test.ts
@@ -15,12 +15,19 @@ describe('packages', () => {
         expect(getVersionInfo()).toBe<string>('Local build');
       });
 
-      it('should return as tag when tag, branch and commit is injected in VersionInfo.ts file', () => {
+      it('should return tag when tag, branch and commit is injected in VersionInfo.ts file', () => {
         VersionInfo.tag = 'v1.0.18';
         VersionInfo.branch = 'main';
         VersionInfo.commit = 'f076db816c6f8f81a42f13dc653711fcb6650f03';
 
         expect(getVersionInfo()).toBe<string>(VersionInfo.tag);
+      });
+
+      it('should return branch name with commit first 7 digits when branch and commit is injected in VersionInfo.ts file', () => {
+        VersionInfo.branch = 'main';
+        VersionInfo.commit = 'f076db816c6f8f81a42f13dc653711fcb6650f03';
+
+        expect(getVersionInfo()).toBe<string>('main [f076db8]');
       });
     });
   });
