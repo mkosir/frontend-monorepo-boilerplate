2371f4bc3c6ef94b4678926c16ca191c08bdc1a2
mkosir <kosir.mar@gmail.com>
2022-10-07 11:48:00 +0200

    chore(deps): update TypeScript

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,21 +1,3 @@
-import path from 'path';
+import { baseStorybookConfig } from 'config-storybook';
 
-import { StorybookConfig } from '@storybook/core-common';
-
-const storybookConfig: StorybookConfig = {
-  stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  staticDirs: ['../public'],
-  typescript: { reactDocgen: 'react-docgen-typescript' },
-  core: {
-    builder: 'webpack5',
-  },
-  webpackFinal: (config) => {
-    // Resolve absolute imports
-    config.resolve?.modules?.push(path.resolve(process.cwd(), 'src'));
-
-    return config;
-  },
-};
-
-module.exports = storybookConfig;
+module.exports = baseStorybookConfig;
