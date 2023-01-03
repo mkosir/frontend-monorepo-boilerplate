efab869759c9ad66e19e89c82371bee7e30029f2
mkosir <kosir.mar@gmail.com>
2023-01-03 21:45:28 +0100

    chore(deps): update dependencies

--- apps/boilerplate-app/next.config.js
+++ apps/boilerplate-app/next.config.js
@@ -3,6 +3,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  modularizeImports: {
+    '@mui/icons-material': {
+      transform: '@mui/icons-material/{{member}}',
+    },
+  },
 };
 
 module.exports = nextConfig;
