590823f9dc281b81474784600efff33d07dadaf4
mkosir <kosir.mar@gmail.com>
2023-06-06 10:09:11 +0200

    chore(deps): update dependencies

--- packages/ui/src/Button/styled.ts
+++ packages/ui/src/Button/styled.ts
@@ -4,7 +4,7 @@ import { shouldForwardProp } from 'utils-mui';
 import { ButtonProps } from './';
 
 export const ButtonStyled = styled(Button, {
-  shouldForwardProp: (prop) => shouldForwardProp<ButtonProps>(['isDisabled', 'bgColor'], prop),
+  shouldForwardProp: shouldForwardProp<ButtonProps>(['isDisabled', 'bgColor']),
 })<ButtonProps>(({ theme, isDisabled, size, bgColor }) => ({
   width: isDisabled ? '100%' : undefined,
   color: size === 'small' ? theme.palette.secondary.main : undefined,
