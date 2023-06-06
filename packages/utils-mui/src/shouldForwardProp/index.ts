de69a264a433f2628bbcd55ceebe13feb6629dc2
mkosir <kosir.mar@gmail.com>
2023-06-06 10:08:12 +0200

    chore(deps): update dependencies

--- packages/utils-mui/src/shouldForwardProp/index.ts
+++ packages/utils-mui/src/shouldForwardProp/index.ts
@@ -1,4 +1,11 @@
-export const shouldForwardProp = <CustomProps extends Record<string, unknown>>(
-  props: Array<keyof CustomProps>,
-  prop: PropertyKey,
-): boolean => !props.includes(prop as string);
+/**
+ * Unknown Prop Warning - https://reactjs.org/warnings/unknown-prop.html
+ */
+
+/**
+ * @param customProps Custom props that shouldn't be passed forward (React warnings as unknown prop).
+ */
+export const shouldForwardProp =
+  <TCustomProps extends Record<string, unknown>>(customProps: ReadonlyArray<keyof TCustomProps>) =>
+  (prop: string): boolean =>
+    !customProps.includes(prop);
--- packages/utils-mui/src/shouldForwardProp/shouldForwardProp.test.ts
+++ packages/utils-mui/src/shouldForwardProp/shouldForwardProp.test.ts
@@ -11,11 +11,11 @@ describe('packages', () => {
   describe('utils-mui', () => {
     describe('shouldForwardProp', () => {
       it('should not forward prop when it exists in custom component props', () => {
-        expect(shouldForwardProp<CustomComponentProps>(['isDisabled', 'bgColor'], 'isDisabled')).toBeFalsy();
+        expect(shouldForwardProp<CustomComponentProps>(['isDisabled', 'bgColor'])('isDisabled')).toBeFalsy();
       });
 
       it('should forward prop when it does not exists in custom component props', () => {
-        expect(shouldForwardProp<CustomComponentProps>(['isDisabled', 'bgColor'], 'notExistingProp')).toBeTruthy();
+        expect(shouldForwardProp<CustomComponentProps>(['isDisabled', 'bgColor'])('notExistingProp')).toBeTruthy();
       });
     });
   });
