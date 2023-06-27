23227bf1711d8ca5b1ec56e72ffd6c41019dd43e
mkosir <kosir.mar@gmail.com>
2023-06-27 10:02:53 +0200

    chore(deps): update dependencies

---
+++ packages/tailwind-config/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ['src/**/*.{ts,tsx}', `${__dirname}/../ui-tailwind/**/*.{ts,tsx}`],
+  content: ['src/**/*.{ts,tsx}'],
+
+  theme: {
+    extend: {
+      colors: {
+        brandBlue: '#243c5a',
+        brandGreen: '#449e48',
+      },
+    },
+  },
+
+  plugins: [],
+};
