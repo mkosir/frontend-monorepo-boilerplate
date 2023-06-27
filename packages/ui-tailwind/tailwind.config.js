f0bd96df342b578aae5cdfe6837e5fb2281e24b4
mkosir <kosir.mar@gmail.com>
2023-06-27 11:03:42 +0200

    chore(deps): update dependencies

--- packages/ui-tailwind/tailwind.config.js
+++ packages/ui-tailwind/tailwind.config.js
@@ -2,4 +2,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('config-tailwind/tailwind.config.js');
 
-module.exports = config;
+module.exports = {
+  // prefix ui lib classes to avoid conflicting with the app
+  prefix: 'ui-',
+  presets: [config],
+};
