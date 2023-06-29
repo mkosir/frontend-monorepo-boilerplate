e25ab4f1b66692c09f34bddac09b573182297242
mkosir <kosir.mar@gmail.com>
2023-06-29 17:24:02 +0200

    chore(deps): update dependencies

--- apps/boilerplate-app/src/modules/Status404Page/index.tsx
+++ apps/boilerplate-app/src/modules/Status404Page/index.tsx
@@ -5,7 +5,7 @@ import { Paths } from 'common/consts/paths';
 export const Status404Page = () => {
   return (
     <div className="mt-24 text-center">
-      <div className="text-xl font-medium">404</div>
+      <div className="text-xl font-medium">404ddd</div>
       <div className="text-md mb-4 mt-1 font-medium">Page not found</div>
       <img src="/assets/missing-piece.png" alt="404 page" className="mx-auto mb-10 w-40" />
       <Link href={Paths.home} className=" text-blue-400 hover:text-blue-500 hover:underline">
