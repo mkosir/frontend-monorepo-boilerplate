f82f4f10680134833ea229483dae5acc766a8242
mkosir <kosir.mar@gmail.com>
2024-02-09 17:40:32 +0100

    chore(deps): update dependencies

--- apps/boilerplate-website/src/modules/AboutPage/index.tsx
+++ apps/boilerplate-website/src/modules/AboutPage/index.tsx
@@ -3,12 +3,14 @@ import { Box, Typography } from '@mui/material';
 export const AboutPage = () => {
   return (
     <Box m={5}>
-      <Typography variant="h5">About Page</Typography>
-      <Box maxWidth={500} mt={2}>
+      <Typography variant="h5" mb={2}>
+        About Page
+      </Typography>
+      <Box maxWidth={500}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+        magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat.Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
         laborum.
       </Box>
     </Box>
