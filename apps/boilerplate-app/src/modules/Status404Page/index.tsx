012a3faa9300e147be3e972caf51507c135ff62b
mkosir <kosir.mar@gmail.com>
2024-02-10 20:12:00 +0100

    chore(deps): update dependencies

--- apps/boilerplate-app/src/modules/Status404Page/index.tsx
+++ apps/boilerplate-app/src/modules/Status404Page/index.tsx
@@ -1,9 +1,9 @@
 export const Status404Page = () => {
   return (
-    <div className="mt-3 flex-col justify-center content-center items-center text-center">
+    <div className="mt-28 text-center">
       <div className="text-xl ">404</div>
       <div className="text-lg mt-1 mb-2">Page not found</div>
-      <img src="/assets/missing-piece.png" alt="404 page" className="w-40" />
+      <img src="/assets/missing-piece.png" alt="404 page" className="mx-auto w-40" />
     </div>
   );
 };
