79523115738ed086147f64375e1e6e7ba4d52d48
mkosir <kosir.mar@gmail.com>
2024-03-03 19:48:55 +0100

    chore(deps): update dependencies

--- packages/utils-mui/src/shouldForwardProp/index.ts
+++ packages/utils-mui/src/shouldForwardProp/index.ts
@@ -1,9 +1,7 @@
 /**
- * Unknown Prop Warning - https://reactjs.org/warnings/unknown-prop.html
- */
-
-/**
- * @param customProps Custom props that shouldn't be passed forward (React warnings as unknown prop).
+ * Utility function that prevents styling props from being passed down and create invalid attribute (React warnings as unknown prop).
+ * @param customProps Custom props that shouldn't be passed forward.
+ * @link Unknown Prop Warning - https://reactjs.org/warnings/unknown-prop.html
  */
 export const shouldForwardProp =
   <TCustomProps extends Record<string, unknown>>(customProps: ReadonlyArray<keyof TCustomProps>) =>
