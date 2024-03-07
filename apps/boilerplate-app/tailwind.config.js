8403dc79986f83dedad9ae2e969b2979a104bef4
mkosir <kosir.mar@gmail.com>
2024-03-07 08:35:33 +0100

    chore(deps): update dependencies

--- apps/boilerplate-app/tailwind.config.js
+++ apps/boilerplate-app/tailwind.config.js
@@ -1,3 +1,8 @@
 import config from 'config-tailwind/tailwind.config.js';
 
-export const presets = [config];
+const tailwindConfig = {
+  presets: [config],
+};
+
+// eslint-disable-next-line import/no-default-export
+export default tailwindConfig;
