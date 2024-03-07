205592c4f68e54861252cc8cfbcedfcf0727981c
mkosir <kosir.mar@gmail.com>
2024-03-07 08:28:18 +0100

    chore(deps): update dependencies

--- packages/ui-tailwind/postcss.config.js
+++ packages/ui-tailwind/postcss.config.js
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('config-tailwind/postcss.config.js');
 
